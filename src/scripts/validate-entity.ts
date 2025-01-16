09e3aa752e324fe37daaf0aa23e0f7c396001c4d
Sergey Kambalin <sergeyk@symbiotic.fi>
2025-01-16 17:09:52 +0600

    SYMB-218 Allow specifying contract addresses in inputs

--- src/scripts/validate-entity.ts
+++ src/scripts/validate-entity.ts
@@ -6,23 +6,12 @@ import * as messages from "./messages";
 
 import { FsValidationResult, EntityType } from "./validate-fs";
 
-type ContractsMap = Partial<Record<EntityType, Address>>;
-
-const chains = [allChains.holesky, allChains.mainnet];
-const entityRegistryContracts: Record<number, ContractsMap> = {
-  [allChains.holesky.id]: {
-    vaults: "0x407a039d94948484d356efb765b3c74382a050b4",
-    networks: "0x7d03b7343bf8d5cec7c0c27ece084a20113d15c9",
-    operators: "0x6f75a4fff97326a00e52662d82ea4fde86a2c548",
-  },
-
-  [allChains.mainnet.id]: {
-    vaults: "0xAEb6bdd95c502390db8f52c8909F703E9Af6a346",
-    networks: "0xC773b1011461e7314CF05f97d95aa8e92C1Fd8aA",
-    operators: "0xAd817a6Bc954F678451A71363f04150FDD81Af9F",
-  },
+type EntityMeta = {
+  contract: string;
+  label: string;
 };
 
+const chains = [allChains.holesky, allChains.mainnet];
 const isEntityAbi = [
   {
     inputs: [{ internalType: "address", name: "entity_", type: "address" }],
@@ -33,24 +22,45 @@ const isEntityAbi = [
   },
 ] as const;
 
+const entityMetaMap: Partial<Record<EntityType, EntityMeta>> = {
+  vaults: {
+    label: "Vault",
+    contract: github.getInput("vault-registry", { required: true }),
+  },
+
+  networks: {
+    label: "Network",
+    contract: github.getInput("network-registry", { required: true }),
+  },
+
+  operators: {
+    label: "Operator",
+    contract: github.getInput("operator-registry", { required: true }),
+  },
+};
+
 export const validateEntity = async ({
   entityType,
   entityId,
 }: FsValidationResult) => {
   const rpcUrl = github.getInput("rpc-url") || undefined;
-  const chainId = +github.getInput("chain-id");
-  const chain = chains.find(({ id }) => id === chainId) || allChains.holesky;
+  const chainId = +github.getInput("chain-id", { required: true });
+  const chain = chains.find(({ id }) => id === chainId);
+
+  if (!chain) {
+    throw new Error(`Chain with id ${chainId} is not supported`);
+  }
+
   const client = createPublicClient({ chain, transport: http(rpcUrl) });
-
   const entityAddress = entityId as Address;
-  const contractAddress = entityRegistryContracts[chain.id][entityType];
+  const entityMeta = entityMetaMap[entityType];
 
-  if (!contractAddress) {
+  if (!entityMeta) {
     return;
   }
 
   const isEntity = await client.readContract({
-    address: contractAddress,
+    address: entityMeta.contract as Address,
     abi: isEntityAbi,
     functionName: "isEntity",
     args: [entityAddress],
@@ -58,11 +68,16 @@ export const validateEntity = async ({
 
   if (!isEntity) {
     await github.addComment(
-      messages.notRegisteredEntity(entityType, entityId, chain.name),
+      messages.notRegisteredEntity(
+        entityMeta.label,
+        entityId,
+        chain.name,
+        entityMeta.contract,
+      ),
     );
 
     throw new Error(
-      `Entity ${entityAddress} is not registered in ${entityType} on ${chain.name}`,
+      `${entityMeta.label} (\`${entityAddress}\`) is not registered on ${chain.name} (\`${entityMeta.contract}\`)`,
     );
   }
 };
